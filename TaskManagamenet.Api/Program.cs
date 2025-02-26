56b9681f45a572c203a7f24cc89b93811ac14c91
Koray Erdem <korayerdem5252@gmail.com>
2025-02-26 09:28:11 +0300

    Initialized project structure, configured PostgreSQL database, and installed required packages
    - Created the initial project structure
    - Configured PostgreSQL connection in appsettings.json and Program.cs
    - Installed Entity Framework Core and Npgsql provider

---
+++ TaskManagamenet.Api/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagamenet.Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
