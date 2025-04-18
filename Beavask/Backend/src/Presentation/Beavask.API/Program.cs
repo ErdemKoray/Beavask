72a211e227d17df471785dd3470dd720e061bfc6
Koray Erdem <korayerdem5252@gmail.com>
2025-04-18 19:57:08 +0300

    user-init added, migration created (InitialCreate), database connection added

--- Beavask/Backend/src/Presentation/Beavask.API/Program.cs
+++ Beavask/Backend/src/Presentation/Beavask.API/Program.cs
@@ -1,21 +1,28 @@
+using Beavask.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var conn = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+builder.Services.AddDbContext<BeavaskDbContext>(options =>
+    options.UseNpgsql(conn));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
-
 app.Run();
-
