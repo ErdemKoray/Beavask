a5626dea55868953ba0f6c110388f0dafb41e8d5
Koray Erdem <korayerdem5252@gmail.com>
2025-04-21 13:23:15 +0300

    Consolidated service and repository registrations using Scrutor

---
+++ Beavask/Backend/src/Infrastructure/Beavask.Persistence/Extensions/DependencyInjection.cs
@@ -0,0 +1,38 @@
+using Beavask.Persistence.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+using Beavask.Application.Interfaces;
+
+namespace Beavask.Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+    {
+        services.AddRepositories();
+        services.AddServices();
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.Scan(scan => scan
+            .FromAssemblyOf<UserRepository>() 
+            .AddClasses(c => c.Where(t => t.Name.EndsWith("Repository")))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.Scan(scan => scan
+            .FromAssemblyOf<IUserContactService>() 
+            .AddClasses(c => c.Where(t => t.Name.EndsWith("Service")))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        return services;
+    }
+}
--- Beavask/Backend/src/Presentation/Beavask.API/Program.cs
+++ Beavask/Backend/src/Presentation/Beavask.API/Program.cs
@@ -1,32 +1,39 @@
 using Beavask.Application.Interfaces;
 using Beavask.Application.Mapping;
+using Beavask.Infrastructure.Extensions;
 using Beavask.Infrastructure.Persistence;
-using Beavask.Persistence.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Connection string
 var conn = builder.Configuration["ConnectionStrings:DefaultConnection"];
 
+// DbContext
 builder.Services.AddDbContext<BeavaskDbContext>(options =>
     options.UseNpgsql(conn));
 
+// DbContext çözümleme (Scrutor uyumlu)
+builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<BeavaskDbContext>());
+
 // DI
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddRepositories();
+builder.Services.AddApplicationDependencies();
 
 // AutoMapper
 builder.Services.AddAutoMapper(Assembly.GetAssembly(typeof(MappingProfile)));
 
-
+// Controller ve Swagger
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Middleware
 if (app.Environment.IsDevelopment())
-{ 
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -36,4 +43,8 @@ else
 }
 
 app.UseHttpsRedirection();
+
+// Controllers route mapping
+app.MapControllers();
+
 app.Run();
