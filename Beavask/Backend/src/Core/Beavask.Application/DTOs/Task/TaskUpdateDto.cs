df6689387f16bfc264bd2c612a6abc58e46367fd
Koray Erdem <korayerdem5252@gmail.com>
2025-05-13 13:49:38 +0300

    Modify TaskUpdateDto

--- Beavask/Backend/src/Core/Beavask.Application/DTOs/Task/TaskUpdateDto.cs
+++ Beavask/Backend/src/Core/Beavask.Application/DTOs/Task/TaskUpdateDto.cs
@@ -9,6 +9,7 @@ public class TaskUpdateDto
     public DateTime? StartDate { get; set; }
     public DateTime? DueDate { get; set; }
     public DateTime? CompletedDate { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public TaskPriority Priority { get; set; }
     public Domain.Enums.TaskStatus Status { get; set; }
     public int? AssignedUserId { get; set; }
