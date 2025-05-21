e53adf70a6cea6ed07fe602569f70917d3cbdb96
Koray Erdem <korayerdem5252@gmail.com>
2025-05-21 18:26:41 +0300

    Modify UserUpdateDto

--- Beavask/Backend/src/Core/Beavask.Application/DTOs/User/UserUpdateDto.cs
+++ Beavask/Backend/src/Core/Beavask.Application/DTOs/User/UserUpdateDto.cs
@@ -5,6 +5,7 @@ namespace Beavask.Application.DTOs.User
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public int CompanyId { get; set; }
         public bool IsActive { get; set; }
     }
 }
